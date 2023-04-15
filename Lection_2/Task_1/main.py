e3fa415ac136c8f5248d96c59d1a991f61735afa
Denis Parashchenko <115428675+VWO0@users.noreply.github.com>
2023-04-15 21:15:02 +0300

    Update
    Text translation.

--- Lection_2/Task_1/main.py
+++ Lection_2/Task_1/main.py
@@ -1,10 +1,10 @@
-n = int(input("Введите количество элементов: "))
+n = int(input("Enter the number of elements: "))
 elements = []
 for i in range(n):
-    element = input(f"Введите элемент {i+1}: ")    
+    element = input(f"Enter element {i+1}: ")    
     elements.append(element)
 
-element_to_count = input("Введите элемент для подсчета: ")
+element_to_count = input("Enter an element to count: ")
 count = elements.count(element_to_count)
 
-print(f"Количество введенных элементов '{element_to_count}': {count}")
+print(f"Number of items entered '{element_to_count}': {count}")
