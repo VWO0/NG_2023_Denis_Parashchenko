9e915f9b029de528c9afc95db1cd410d65c1d82c
Denis Parashchenko <115428675+VWO0@users.noreply.github.com>
2023-11-26 19:37:43 +0300

    Update main.py
    The source code has been simplified by introducing two separate functions: celsius_to_fahrenheit and fahrenheit_to_celsius, each responsible for converting temperatures between Celsius and Fahrenheit.

--- Lection 1/task 3/main.py
+++ Lection 1/task 3/main.py
@@ -1,17 +1,22 @@
-print ("1. Convert from Celsius to Fahrenheit.")
-print ("2. Convert from Fahrenheit to Celsius.")
-operations = input("Select operations 1 or 2.")
-operations = int(operations)
+def celsius_to_fahrenheit(celsius):
+    return celsius * 1.8 + 32
 
-if operations == 1:
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) / 1.8
+
+print("1. Convert from Celsius to Fahrenheit.")
+print("2. Convert from Fahrenheit to Celsius.")
+operation = input("Select operation 1 or 2: ")
+
+if operation == '1':
     celsius = float(input("Enter temperature in degrees Celsius: "))
-    fahrenheit = celsius * 1.8 + 32
-    print ("Result:",fahrenheit)
+    result = celsius_to_fahrenheit(celsius)
+    print(f"Result: {result}°F")
 
-elif operations == 2:
+elif operation == '2':
     fahrenheit = float(input("Enter temperature in degrees Fahrenheit: "))
-    celsius = (fahrenheit - 32) /1,8
-    print ("Result:",celsius)
+    result = fahrenheit_to_celsius(fahrenheit)
+    print(f"Result: {result}°C")
 
 else:
-    print ("Incorrect choice. Please enter 1 or 2.")
+    print("Incorrect choice. Please enter 1 or 2.")
