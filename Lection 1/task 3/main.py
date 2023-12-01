8754c98d70735edc8666b1bae08498ee156da1c3
VWO0 <denus.parashencko@gmail.com>
2023-12-01 23:19:55 +0200

    Simplified the code

--- Lection 1/task 3/main.py
+++ Lection 1/task 3/main.py
@@ -1,20 +1,17 @@
-def celsius_to_fahrenheit(celsius):
-    return celsius * 1.8 + 32
+def celsius_to_fahrenheit(data):
+    return data * 1.8 + 32
 
-def fahrenheit_to_celsius(fahrenheit):
-    return (fahrenheit - 32) / 1.8
+def fahrenheit_to_celsius(data):
+    return (data - 32) / 1.8
 
-print("1. Convert from Celsius to Fahrenheit.")
-print("2. Convert from Fahrenheit to Celsius.")
+print("1. Convert from Celsius to Fahrenheit.","2. Convert from Fahrenheit to Celsius.")
 operation = input("Select operation 1 or 2: ")
-
+data =float(input("Enter temperature"))
 if operation == '1':
-    celsius = float(input("Enter temperature in degrees Celsius: "))
     result = celsius_to_fahrenheit(celsius)
     print(f"Result: {result}°F")
 
 elif operation == '2':
-    fahrenheit = float(input("Enter temperature in degrees Fahrenheit: "))
     result = fahrenheit_to_celsius(fahrenheit)
     print(f"Result: {result}°C")
 
